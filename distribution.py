6e052fc35aee11e282038e2b7cdb2a2c336ed728
Eric Dennison <ericd@netdenizen.com>
2015-09-23 11:27:58 -0400

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -5,9 +5,34 @@ Credit: <list sources used, if any>
 
 Assignment:
 
-Your program will ask the user the following questions, in this order:
-1. Their name.
-2. The name of the month they were born in (e.g. "September").
-3. The year they were born in (e.g. "1962").
-4. The day they were born on (e.g. "11").
+Write and submit a Python program (distribution.py) that computes and displays 
+the distribution of characters in a given sample of text.
+
+Output of your program should look like this:
+
+Please enter a string of text (the bigger the better): The rain in Spain stays mainly in the plain.
+The distribution of characters in "The rain in Spain stays mainly in the plain." is:
+iiiiii
+nnnnnn
+aaaaa
+sss
+ttt
+ee
+hh
+ll
+pp
+yy
+m
+r
+
+Notice about this example:
+
+* The text: 'The rain ... plain' is provided by the user as input to your program.
+* Uppercase characters are converted to lowercase
+* Spaces and punctuation marks are ignored completely.
+* Characters that are more common appear first in the list.
+* Where the same number of characters occur, the lines are ordered alphabetically. 
+  For example, in the printout above, the letters e, h, l, p and y both occur twice 
+  in the text and they are listed in the output in alphabetical order.
+* Letters that do not occur in the text are not listed in the output at all.
 """
