0c30fe8d7ab3b1d34cbc7e13e7fbf8247b2ae3a7
Eric Dennison <ericd@netdenizen.com>
2015-09-23 11:26:02 -0400

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -1,1 +1,13 @@
-placeholder
+"""
+distribution.py
+Author: <your name here>
+Credit: <list sources used, if any>
+
+Assignment:
+
+Your program will ask the user the following questions, in this order:
+1. Their name.
+2. The name of the month they were born in (e.g. "September").
+3. The year they were born in (e.g. "1962").
+4. The day they were born on (e.g. "11").
+"""
