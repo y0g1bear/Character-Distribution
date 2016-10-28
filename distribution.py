711aa4f1e7159141c75c73e41da3e8fbb7d477d6
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-10-28 13:40:09 -0400

    Updated from Brython Server: 10/28/2016 1:40:09 PM

--- distribution.py
+++ distribution.py
@@ -36,3 +36,8 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+we = input("Please enter a string of text (the bigger the better): ")
+harambe = ('The ditribution of characters in "'  + we +  '" is:')
+print(harambe)
+
+wesplit = we.split()
