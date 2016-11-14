f066ce3435840e3a14123afad5f3eda12abf1803
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-11-14 13:57:09 -0500

    Updated from Brython Server: 11/14/2016 1:57:16 PM

--- distribution.py
+++ distribution.py
@@ -41,7 +41,7 @@ harambe = ('The ditribution of characters in "'  + we +  '" is:')
 print(harambe)
 letters = []
 wesplit = we.split()
-alph = ('a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+alph = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 for e in alph:
     school = we.count(e)
     if not school == 0:
